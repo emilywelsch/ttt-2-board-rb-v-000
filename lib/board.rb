24559f3bbf185f4041c0c96bd015194959dea7bf
Emily Welsch <emwelsch@gmail.com>
2018-01-24 14:23:09 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,16 @@
 # Define the variable board below.
 board = [" ", " ", " "," ", " ", " "," ", " ", " "]
+def print_name
+  puts "Emily Welsch"
+end
+print_name
+def restaurant
+  restaurant_name = "Guy's American Kitchen & Bar"
+  cuisine = "american"
+  motto = "Welcome to Flavor Town!"
+end
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
